70b46b77f7f046c0c4814043fac0734140175543
BasPennings <p.freekje@gmail.com>
2025-04-20 21:28:27 +0200

    #4 Finished player controller with new input system

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using static UnityEngine.InputSystem.InputAction;
 
 [RequireComponent(typeof(Rigidbody2D), typeof(Collider2D))]
 public class PlayerController : MonoBehaviour
@@ -8,10 +9,28 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody2D rb;
     private CapsuleCollider2D col;
+    private InputSystem_Actions controls;
+    private InputSystem_Actions.PlayerActions playerActions;
+    private Action<CallbackContext> onJumpPerformed;
+    private Action<CallbackContext> onJumpCanceled;
+    private Action<CallbackContext> onMovePerformed;
+    private Action<CallbackContext> onMoveCanceled;
     private FrameInput frameInput;
     private Vector2 frameVelocity;
+    private Vector2 inputMovementVector;
     private bool cachedQueryStartInColliders;
+    private bool jumpToConsume;
+    private bool bufferedJumpUsable;
+    private bool endedJumpEarly;
+    private bool coyoteUsable;
+    private bool grounded;
+    private bool isJumpBeingHeld = false;
     private float time;
+    private float timeJumpWasPressed;
+    private float frameLeftGrounded = float.MinValue;
+
+    private bool HasBufferedJump => bufferedJumpUsable && time < timeJumpWasPressed + _settings.JumpBuffer;
+    private bool CanUseCoyote => coyoteUsable && !grounded && time < frameLeftGrounded + _settings.CoyoteTime;
 
     private void Awake()
     {
@@ -19,7 +38,24 @@ public class PlayerController : MonoBehaviour
         col = GetComponent<CapsuleCollider2D>();
 
         cachedQueryStartInColliders = Physics2D.queriesStartInColliders;
+
+        controls = new();
+        playerActions = controls.Player;
+
+        onJumpPerformed = ctx => isJumpBeingHeld = true;
+        onJumpCanceled = ctx => isJumpBeingHeld = false;
+
+        onMovePerformed = ctx => inputMovementVector = ctx.ReadValue<Vector2>();
+        onMoveCanceled = ctx => inputMovementVector = Vector2.zero;
+
+        RegisterInputCallbacks();
     }
+
+    private void OnEnable() => playerActions.Enable();
+
+    private void OnDisable() => playerActions.Disable();
+
+    private void OnDestroy() => UnRegisterInputCallbacks();
 
     private void Update()
     {
@@ -27,13 +63,40 @@ public class PlayerController : MonoBehaviour
         GatherInput();
     }
 
+    private void FixedUpdate()
+    {
+        CheckCollisions();
+
+        HandleJump();
+        HandleDirection();
+        HandleGravity();
+
+        ApplyMovement();
+    }
+
+    private void RegisterInputCallbacks()
+    {
+        playerActions.Jump.performed += onJumpPerformed;
+        playerActions.Jump.canceled += onJumpCanceled;
+        playerActions.Move.performed += onMovePerformed;
+        playerActions.Move.canceled += onMoveCanceled;
+    }
+
+    private void UnRegisterInputCallbacks()
+    {
+        playerActions.Jump.performed -= onJumpPerformed;
+        playerActions.Jump.canceled -= onJumpCanceled;
+        playerActions.Move.performed -= onMovePerformed;
+        playerActions.Move.canceled -= onMoveCanceled;
+    }
+
     private void GatherInput()
     {
         frameInput = new FrameInput
         {
-            IsJumpingDown = Input.GetButtonDown("Jump") || Input.GetKeyDown(KeyCode.C),
-            IsJumpHeld = Input.GetButton("Jump") || Input.GetKey(KeyCode.C),
-            WASD = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"))
+            IsJumpingDown = playerActions.Jump.WasPressedThisFrame(),
+            IsJumpHeld = isJumpBeingHeld,
+            WASD = inputMovementVector
         };
 
         if (_settings.SnapInput)
@@ -44,26 +107,10 @@ public class PlayerController : MonoBehaviour
 
         if (frameInput.IsJumpingDown)
         {
-            _jumpToConsume = true;
-            _timeJumpWasPressed = time;
+            jumpToConsume = true;
+            timeJumpWasPressed = time;
         }
     }
-
-    private void FixedUpdate()
-    {
-        CheckCollisions();
-
-        HandleJump();
-        HandleDirection();
-        HandleGravity();
-            
-        ApplyMovement();
-    }
-
-    #region Collisions
-        
-    private float _frameLeftGrounded = float.MinValue;
-    private bool _grounded;
 
     private void CheckCollisions()
     {
@@ -77,92 +124,63 @@ public class PlayerController : MonoBehaviour
         if (ceilingHit) frameVelocity.y = Mathf.Min(0, frameVelocity.y);
 
         // Landed on the Ground
-        if (!_grounded && groundHit)
+        if (!grounded && groundHit)
         {
-            _grounded = true;
-            _coyoteUsable = true;
-            _bufferedJumpUsable = true;
-            _endedJumpEarly = false;
+            grounded = true;
+            coyoteUsable = true;
+            bufferedJumpUsable = true;
+            endedJumpEarly = false;
         }
         // Left the Ground
-        else if (_grounded && !groundHit)
+        else if (grounded && !groundHit)
         {
-            _grounded = false;
-            _frameLeftGrounded = time;
+            grounded = false;
+            frameLeftGrounded = time;
         }
 
         Physics2D.queriesStartInColliders = cachedQueryStartInColliders;
     }
 
-    #endregion
-
-    #region Jumping
-
-    private bool _jumpToConsume;
-    private bool _bufferedJumpUsable;
-    private bool _endedJumpEarly;
-    private bool _coyoteUsable;
-    private float _timeJumpWasPressed;
-
-    private bool HasBufferedJump => _bufferedJumpUsable && time < _timeJumpWasPressed + _settings.JumpBuffer;
-    private bool CanUseCoyote => _coyoteUsable && !_grounded && time < _frameLeftGrounded + _settings.CoyoteTime;
-
     private void HandleJump()
     {
-        if (!_endedJumpEarly && !_grounded && !frameInput.IsJumpHeld && rb.linearVelocity.y > 0) _endedJumpEarly = true;
+        if (!endedJumpEarly && !grounded && !frameInput.IsJumpHeld && rb.linearVelocity.y > 0) endedJumpEarly = true;
 
-        if (!_jumpToConsume && !HasBufferedJump) return;
+        if (!jumpToConsume && !HasBufferedJump) return;
 
-        if (_grounded || CanUseCoyote) ExecuteJump();
+        if (grounded || CanUseCoyote) ExecuteJump();
 
-        _jumpToConsume = false;
+        jumpToConsume = false;
     }
 
     private void ExecuteJump()
     {
-        _endedJumpEarly = false;
-        _timeJumpWasPressed = 0;
-        _bufferedJumpUsable = false;
-        _coyoteUsable = false;
+        endedJumpEarly = false;
+        timeJumpWasPressed = 0;
+        bufferedJumpUsable = false;
+        coyoteUsable = false;
         frameVelocity.y = _settings.JumpPower;
     }
-
-    #endregion
-
-    #region Horizontal
 
     private void HandleDirection()
     {
         if (frameInput.WASD.x == 0)
         {
-            var deceleration = _grounded ? _settings.GroundDeceleration : _settings.AirDeceleration;
+            var deceleration = grounded ? _settings.GroundDeceleration : _settings.AirDeceleration;
             frameVelocity.x = Mathf.MoveTowards(frameVelocity.x, 0, deceleration * Time.fixedDeltaTime);
         }
-        else
-        {
-            frameVelocity.x = Mathf.MoveTowards(frameVelocity.x, frameInput.WASD.x * _settings.MaxSpeed, _settings.Acceleration * Time.fixedDeltaTime);
-        }
+        else frameVelocity.x = Mathf.MoveTowards(frameVelocity.x, frameInput.WASD.x * _settings.MaxSpeed, _settings.Acceleration * Time.fixedDeltaTime);
     }
-
-    #endregion
-
-    #region Gravity
 
     private void HandleGravity()
     {
-        if (_grounded && frameVelocity.y <= 0f)
-        {
-            frameVelocity.y = _settings.GroundingForce;
-        }
+        if (grounded && frameVelocity.y <= 0f) frameVelocity.y = _settings.GroundingForce;
         else
         {
             var inAirGravity = _settings.FallAcceleration;
-            if (_endedJumpEarly && frameVelocity.y > 0) inAirGravity *= _settings.JumpEndEarlyGravityModifier;
+            if (endedJumpEarly && frameVelocity.y > 0) inAirGravity *= _settings.JumpEndEarlyGravityModifier;
             frameVelocity.y = Mathf.MoveTowards(frameVelocity.y, -_settings.MaxFallSpeed, inAirGravity * Time.fixedDeltaTime);
         }
     }
-
-    #endregion
 
     private void ApplyMovement() => rb.linearVelocity = frameVelocity;
 }
